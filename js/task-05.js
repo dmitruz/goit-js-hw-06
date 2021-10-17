0827ec6500f28f7a87d37d297a4b30cb4a84ce9f
dmitruz <dmitruz970@gmail.com>
2021-10-17 16:17:44 +0300

    Finished 5 quiz

--- js/task-05.js
+++ js/task-05.js
@@ -1,24 +1,16 @@
-/*const refs = {
+
+
+const result = {
     input: document.querySelector('#name-input'),
     span: document.querySelector('#name-output'),
 }
 
-refs.input.addEventListener('input', onInputChange);
+result.input.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
-    refs.span.textContent = event.currentTarget.value;
-    if (refs.input.value.trim() === "".trim()) {
-        refs.span.textContent = "Anonymous";
+    result.span.textContent = event.currentTarget.value;
+    if (result.input.value.trim() === "".trim()) {
+        result.span.textContent = "Anonymous";
     }
     console.log(event.currentTarget.value);
 }
-*/
-const input = document.querySelector('#name-input');
-const span = document.querySelector('#name-input');
-function clickInput () {
-    if (input.value === "") {
-        return span.textContent = 'Anonymous';
-    }
-        return span.textCont = input.value;   
-}
-input.addEventListener('input', onclick)
--- js/task11.js
+++
@@ -1,2 +0,0 @@
-const a = document.querySelector('#categories');
-const b = document.querySelectorAll('item');
